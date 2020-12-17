0736a57c756a6ea0cb777b1abf8aec0bf37655a4
Juan Felipe Coronel <jfmcoronel@gmail.com>
2020-12-18 00:24:02 +0800

    Fix typo

--- fuzz/afl/afl-fuzz.c
+++ fuzz/afl/afl-fuzz.c
@@ -5032,8 +5032,8 @@ EXP_ST u8 common_fuzz_stuff(char** argv, u8* out_buf, u32 len) {
   // EEVEE: Save all fuzz inputs
   fuzz_input_ctr++;
   u8 *fn = alloc_printf("%s/inputs/%06d.js", out_dir, fuzz_input_ctr);
-  int fd = open(tmp, O_WRONLY | O_CREAT | O_EXCL, 0600);
-  if (fd < 0) PFATAL("Unable to create '%s'", tmp);
+  int fd = open(fn, O_WRONLY | O_CREAT | O_EXCL, 0600);
+  if (fd < 0) PFATAL("Unable to create '%s'", fn);
   ck_free(fn);
   FILE *fp = fdopen(fd, "w");
   if (!fp) PFATAL("fdopen() failed");
