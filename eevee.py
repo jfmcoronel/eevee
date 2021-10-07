60df6eca44d2144d1ca39c7ef6f5673f33c86cd7
Juan Felipe Coronel <jfmcoronel@gmail.com>
2021-10-07 12:31:47 +0800

    Add v8 support

--- eevee.py
+++ eevee.py
@@ -1,35 +1,32 @@
 import os
 from collections import Counter
 
-JSC_ENV = "JSC_useConcurrentJIT=false JSC_logCompilationChanges=true"
-JSC_BIN_PATH = "timeout 10 /home/jfmcoronel/jsc"
+V8_ENV = ""
+V8_BIN_PATH = "timeout 10 /home/jfmcoronel/v8-/v8/out/x64.debug/d8 --trace-turbo-reduction"
 CURRENT_INPUT_PATH = "/home/jfmcoronel/die/output-0/.cur_input"
 OUTPUT_FILEPATH = "/home/jfmcoronel/die/output-0/.eevee_dump"
 
-JSC_CMD = f"{JSC_ENV} {JSC_BIN_PATH} {CURRENT_INPUT_PATH} 2> {OUTPUT_FILEPATH}"
+V8_CMD = f"{V8_ENV} {V8_BIN_PATH} {CURRENT_INPUT_PATH} 2> {OUTPUT_FILEPATH}"
 
 
 def emit_key():
+    ctr = Counter()
+
     with open(OUTPUT_FILEPATH, "r") as f:
         lines = f.readlines()
 
-    ctr = Counter()
-
     for line in lines:
-        line = line.strip()
-        if "changed the IR" in line:
-            ctr[line] += 1
+        reducer = line.rsplit(' ', maxsplit=1)[-1].strip()
+        ctr[reducer] += 1
 
     key_parts = []
     for key in ctr:
-        new_key = key.replace("Phase ", "").replace(" changed the IR.", "").replace(" ", "")
-        key_parts.append(f"{new_key}{ctr[key]}")
+        key = key.replace("Phase ", "").replace(" changed the IR.", "").replace(" ", "")
+        key_parts.append(f"{key}{ctr[key]}")
 
-    key = "".join(key_parts) + "\0"
-
-    with open(OUTPUT_FILEPATH, "w") as f:
-        f.write(key)
+    final_key = "".join(key_parts) + "\0"
+    print(final_key)
 
 
-os.system(JSC_CMD)
+os.system(V8_CMD)
 emit_key()
