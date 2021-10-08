eb94d4c7cb400a44f562deec4d33666e537bd2df
Juan Felipe Coronel <juanfelipe.coronel@gmail.com>
2021-10-09 02:56:44 +0800

    Update eevee.py

--- eevee.py
+++ eevee.py
@@ -2,7 +2,7 @@ import os
 from collections import Counter
 
 V8_ENV = ""
-V8_BIN_PATH = "timeout 10 /home/jfmcoronel/die/engines/v8/v8/out/gcovbuild/d8 --trace-turbo-reduction"
+V8_BIN_PATH = "timeout 10 /home/jfmcoronel/die/engines/v8/v8/out/x64.release/d8 --trace-turbo-reduction"
 CURRENT_INPUT_PATH = "/home/jfmcoronel/die/output-0/.cur_input"
 OUTPUT_FILEPATH = "/home/jfmcoronel/die/output-0/.eevee_dump"
 
