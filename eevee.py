30aecd8c12adb3b9b2a15dd1effb2a45c9407529
Juan Felipe Coronel <juanfelipe.coronel@gmail.com>
2021-10-09 11:15:13 +0800

    Update eevee.py

--- eevee.py
+++ eevee.py
@@ -6,7 +6,7 @@ V8_BIN_PATH = "timeout 10 /home/jfmcoronel/die/engines/v8/v8/out/x64.release/d8 
 CURRENT_INPUT_PATH = "/home/jfmcoronel/die/output-0/.cur_input"
 OUTPUT_FILEPATH = "/home/jfmcoronel/die/output-0/.eevee_dump"
 
-V8_CMD = f"{V8_ENV} {V8_BIN_PATH} {CURRENT_INPUT_PATH} 2> {OUTPUT_FILEPATH}"
+V8_CMD = f"{V8_ENV} {V8_BIN_PATH} {CURRENT_INPUT_PATH} 2>&1 > {OUTPUT_FILEPATH}"
 
 
 def emit_key():
