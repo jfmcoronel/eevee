d8a5c518626c9c0ea31dffa191a1cdf8d7817dbe
Juan Felipe Coronel <jfmcoronel@gmail.com>
2021-11-11 10:42:30 +0800

    Make run-all.py parallel again

--- fuzz/scripts/run-all.py
+++ fuzz/scripts/run-all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
-import os
 import multiprocessing
+import os
 from os import environ
 
 environ["REDIS_URL"] = "redis://localhost:9000"
@@ -10,7 +10,7 @@ environ["REDIS_URL"] = "redis://localhost:9000"
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('cmd', nargs='+')
-    p.add_argument('--cpu', nargs='?', type=int, default=1)
+    p.add_argument('--cpu', nargs='?', type=int, default=int(multiprocessing.cpu_count() - 4))
     cmd = p.parse_args().cmd
     assert('output' in cmd)
     for i in range(p.parse_args().cpu):
