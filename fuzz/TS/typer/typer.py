fc93836ec732efe0eac93052d95db25dd25ba2d5
Juan Felipe Coronel <jfmcoronel@gmail.com>
2021-11-11 12:40:16 +0800

    Change d8 path for typer.py

--- fuzz/TS/typer/typer.py
+++ fuzz/TS/typer/typer.py
@@ -1,10 +1,15 @@
-import subprocess, os, sys, tempfile, json
+import json
+import os
+import subprocess
+import sys
+import tempfile
+
 import resolve
 
 # I prefer to use d8 to run the instrumented code, 
 # which should be fastest I believe.
 
-d8_path = "/home/jfmcoronel/v8-root/v8/out/x64.debug/d8"
+d8_path = "/home/jfmcoronel/v8-root/v8/out/x64.release/d8"
 
 def get_lib(path):
     if path.find("ChakraCore/") != -1 or path.find("chakra/") != -1:
