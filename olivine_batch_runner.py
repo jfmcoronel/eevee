51f594b27f935bbeb206378c492bfb3d1577422f
Juan Felipe Coronel <jfmcoronel@gmail.com>
2021-11-12 12:18:54 +0800

    Fix lost logs

--- olivine_batch_runner.py
+++ olivine_batch_runner.py
@@ -86,7 +86,7 @@ def populate(jit_compiler_code: str, until_n_inputs: int, seed: int):
 def populate_with_slave(n: str, fuzz_target_path: str, jit_compiler_code: str, until_n_inputs: int, seed: int):
     lib_string = get_lib_string(jit_compiler_code)
 
-    execute(f'''bash -c "{{ time ./fuzz/afl/afl-fuzz -s {seed} -e {until_n_inputs} -m none -o output-{n} -i ./corpus/output-{n} '{fuzz_target_path}' {lib_string} @@ ; }} 2> >(tee ~/die/output-{n}/time-populate.txt >&2)"''')
+    execute(f'''bash -c "{{ time ./fuzz/afl/afl-fuzz -s {seed} -e {until_n_inputs} -m none -o output-{n} -i ./corpus/output-{n} '{fuzz_target_path}' {lib_string} @@ ; }} 2> >(tee ~/die/output-{n}/time-populate.txt >&2) >&2"''')
 
 
 def fuzz(jit_compiler_code: str, until_n_inputs: int, seed: int):
@@ -97,9 +97,9 @@ def fuzz(jit_compiler_code: str, until_n_inputs: int, seed: int):
 
     cmd: list[str] = [
         'cd ~/die',
-        f'''bash -c "{{{{ time ./fuzz/afl/afl-fuzz -s {seed} -e {until_n_inputs} -j {jit_compiler_code} -m none -o output '{fuzz_target_path}' {lib_string} @@ ; }}}} 2> >(tee ~/die/output/time-fuzz.txt >&2)"''',
-        f'bash -c "{{{{ time python3 ~/die/olivine_slave_analysis.py optset {{SLAVENUMBER}} {jit_compiler_code} ; }}}} 2> >(tee ~/die/output/time-analyze-optset.txt >&2)"',
-        f'bash -c "{{{{ time python3 ~/die/olivine_slave_analysis.py coverage {{SLAVENUMBER}} {jit_compiler_code} ; }}}} 2> >(tee ~/die/output/time-analyze-coverage.txt >&2)"',
+        f'''bash -c "{{{{ time ./fuzz/afl/afl-fuzz -s {seed} -e {until_n_inputs} -j {jit_compiler_code} -m none -o output '{fuzz_target_path}' {lib_string} @@ ; }}}} 2> >(tee ~/die/output/time-fuzz.txt >&2) >&2"''',
+        f'bash -c "{{{{ time python3 ~/die/olivine_slave_analysis.py optset {{SLAVENUMBER}} {jit_compiler_code} ; }}}} 2> >(tee ~/die/output/time-analyze-optset.txt >&2) >&2"',
+        f'bash -c "{{{{ time python3 ~/die/olivine_slave_analysis.py coverage {{SLAVENUMBER}} {jit_compiler_code} ; }}}} 2> >(tee ~/die/output/time-analyze-coverage.txt >&2) >&2"',
     ]
 
     run_windowed_slaves_in_current_session(' ; '.join(cmd), 'fuzz', True)
